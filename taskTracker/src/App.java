66f200151ecccbe197a7d99b5d3719051a37e42b
haramurti <phneval@gmail.com>
2025-12-12 23:58:03 +0700

    adding my own string builder for JSON files

--- taskTracker/src/App.java
+++ taskTracker/src/App.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.Instant;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
 public class App {
-    public void printtoJSON() {
-        System.out.println("printing into json");
-    }
+
+    public static ArrayList<Task> listTasks = new ArrayList<Task>();
+
+
     public static void main(String[] args) throws Exception {
         String status1 = "to-do";
         String status2 = "in-progress";
         String status3 = "done";
         Scanner in = new Scanner(System.in);
-        ArrayList<Task> listTasks = new ArrayList<Task>();
         while (true) {
             String input = in.next();
             //add
@@ -19,9 +23,13 @@ public class App {
                 Task task = new Task(desc);
                 listTasks.add(task);
                 System.out.println("success adding task with id : " + task.getId() + "\n description : " + desc);
+
+                saveDataJSON(listTasks);
+
                 //update
             } else if (input.equals("update")) {
                 int idTujuan = in.nextInt();
+                in.nextLine();
                 String updateInput = in.nextLine();
                 boolean flag = false;
                 for (int i = 0; i < listTasks.size(); i++) {;
@@ -29,6 +37,7 @@ public class App {
                         int id_target = listTasks.get(i).getId();
                         listTasks.get(i).setDesc(updateInput);;
                         System.out.println("succesfully updated task with id : " + id_target);
+                        saveDataJSON(listTasks);
                         flag = true;
                         break;
                     }
@@ -39,12 +48,14 @@ public class App {
                 //delete
             } else if (input.equals("delete")) {
                 int idTujuan = in.nextInt();
+in.nextLine();
                 boolean flag = false;
                 for (int i = 0; i < listTasks.size(); i++) {;
                     if (idTujuan == listTasks.get(i).getId()) {
                         int id_target = listTasks.get(i).getId();
                         listTasks.remove(i);
                         System.out.println("succesfully deleted task with id : " + id_target);
+                        saveDataJSON(listTasks);
                         flag = true;
                         break;
                     }
@@ -54,12 +65,14 @@ public class App {
                 }
             } else if (input.equals("mark-in-progress")) {
                 int idTujuan = in.nextInt();
+in.nextLine();
                 boolean flag = false;
                 for (int i = 0; i < listTasks.size(); i++) {;
                     if (idTujuan == listTasks.get(i).getId()) {
                         int id_target = listTasks.get(i).getId();
                         listTasks.get(i).setStatus(status2);
                         System.out.println("Task " + id_target + " marked as 'in-progress'");
+                        saveDataJSON(listTasks);
                         flag = true;
                         break;
                     }
@@ -69,12 +82,14 @@ public class App {
                 }
             } else if (input.equals("mark-done")) {
                 int idTujuan = in.nextInt();
+in.nextLine();
                 boolean flag = false;
                 for (int i = 0; i < listTasks.size(); i++) {;
                     if (idTujuan == listTasks.get(i).getId()) {
                         int id_target = listTasks.get(i).getId();
                         listTasks.get(i).setStatus(status3);
                         System.out.println("Task " + id_target + " marked as 'done'");
+                        saveDataJSON(listTasks);
                         flag = true;
                         break;
                     }
@@ -84,12 +99,14 @@ public class App {
                 }
             } else if (input.equals("to-do")) {
                 int idTujuan = in.nextInt();
+in.nextLine();
                 boolean flag = false;
                 for (int i = 0; i < listTasks.size(); i++) {;
                     if (idTujuan == listTasks.get(i).getId()) {
                         int id_target = listTasks.get(i).getId();
                         listTasks.get(i).setStatus(status1);
                         System.out.println("Task " + id_target + " marked as 'to-do'");
+                        saveDataJSON(listTasks);
                         flag = true;
                         break;
                     }
@@ -103,5 +120,38 @@ public class App {
                 }
             }
         }
+    }
+
+
+    public static void saveDataJSON(ArrayList<Task> tasks) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[\n");
+
+        for (int i = 0; i< listTasks.size(); i++){
+
+            Task t = listTasks.get(i);
+            sb.append("  {\n");
+            sb.append("    \"id\": ").append(t.getId()).append(",\n");
+
+            String amanDesc = t.getDesc().replace("\"", "\\\"");
+            sb.append("    \"description\": \"").append(amanDesc).append("\",\n");
+
+            sb.append("    \"status\": \"").append(t.getStatus()).append("\",\n");
+
+            sb.append("    \"CreatedAt\": \"").append(t.getCreatedaAt().toString()).append("\",\n");
+
+            sb.append("    \"UpdatedAt\": \"").append(t.getUpdatedAt().toString()).append("\"\n");
+            sb.append("  }");
+        }
+        sb.append("]");
+
+        try {
+            Files.writeString(Paths.get("tasks.json"),sb.toString());
+            System.out.println("succesfully saved");
+        }catch (IOException e ){
+            System.out.println("failed save file " + e.getMessage());
+        }
+
     }
 }
